7a2583d5354bef86bbfcfd3609c0f824891784a8
nickiiinicole <nicki.nicoledc04gmail.com>
2026-05-20 22:58:42 +0200

    no puedo mas

---
+++ exam/StandartScaler.py
@@ -0,0 +1,37 @@
+import pandas as pd # type ignroe
+from torch.utils.data import Dataset 
+import torch
+from torch.utils.data import random_split
+import torch.nn.functional as F
+import torch.nn as nn
+from csv import writer
+import matplotlib.pyplot as plt
+
+class StandardScaler:
+
+    def __init__(self, mean=None, std=None, epsilon=1e-7):
+        """Standard Scaler.
+        The class can be used to normalize PyTorch Tensors using native functions. The module does not expect the
+        tensors to be of any specific shape; as long as the features are the last dimension in the tensor, the module
+        will work fine.
+        :param mean: The mean of the features. The property will be set after a call to fit.
+        :param std: The standard deviation of the features. The property will be set after a call to fit.
+        :param epsilon: Used to avoid a Division-By-Zero exception.
+        """
+        self.mean = mean
+        self.std = std
+        self.epsilon = epsilon
+    def fit(self, values):
+        dims = list(range(values.dim() - 1))
+        self.mean = torch.mean(values, dim=dims)
+        self.std = torch.std(values, dim=dims)
+
+    def transform(self, values):
+        return (values - self.mean) / (self.std + self.epsilon)
+
+    def fit_transform(self, values):
+        self.fit(values)
+        return self.transform(values)
+
+    def __repr__(self):
+        return f"mean: {self.mean}, std:{self.std}, epsilon:{self.epsilon}"
